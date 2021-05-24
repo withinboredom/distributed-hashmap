f85c00d1907270c1976dbd4551964010f6188764
Rob Landers <landers.robert@gmail.com>
2021-05-24 16:46:29 +0200

    Enable csharp

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 
 docker-compose down -v
 docker-compose up -d php-daprd csharp-daprd
-docker-compose build php-writer
-#docker-compose run csharp-writer
+docker-compose build php-writer csharp-writer
+docker-compose run csharp-writer
 docker-compose run php-writer
