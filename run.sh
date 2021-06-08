156c23c873813d1f735ab10a170947b03727059f
Rob Landers <landers.robert@gmail.com>
2021-06-08 23:03:56 +0200

    Log subscription output
    Signed-off-by: Rob Landers <landers.robert@gmail.com>

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ set -e
 docker-compose down -v
 docker-compose build --pull subscriptions
 docker-compose up -d php-daprd csharp-daprd subscriptions
+docker-compose logs -f subscriptions &
 docker-compose build --pull php-writer csharp-writer php-reader csharp-reader php-unit php-sub-validator
 docker-compose run php-unit
-docker-compose run csharp-writer
 docker-compose run php-writer
+docker-compose run csharp-writer
 docker-compose run php-reader
 docker-compose run csharp-reader
 docker-compose run php-sub-validator
