f9f4213c6b5a27b317608cb9d81ba4577917fc18
Rob Landers <landers.robert@gmail.com>
2021-06-08 22:56:48 +0200

    Do not do parallel
    Signed-off-by: Rob Landers <landers.robert@gmail.com>

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 docker-compose down -v
 docker-compose build --pull subscriptions
 docker-compose up -d php-daprd csharp-daprd subscriptions
-docker-compose build --pull --parallel php-writer csharp-writer php-reader csharp-reader php-unit php-sub-validator
+docker-compose build --pull php-writer csharp-writer php-reader csharp-reader php-unit php-sub-validator
 docker-compose run php-unit
 docker-compose run csharp-writer
 docker-compose run php-writer
