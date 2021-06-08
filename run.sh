4c4667b7495ed5c9f6c08256cf209c1ab6454c43
Rob Landers <landers.robert@gmail.com>
2021-06-08 23:11:05 +0200

    wait awhile for all events to be received
    Signed-off-by: Rob Landers <landers.robert@gmail.com>

--- run.sh
+++ run.sh
@@ -4,11 +4,14 @@ set -e
 docker-compose down -v
 docker-compose build --pull subscriptions
 docker-compose up -d php-daprd csharp-daprd subscriptions
-docker-compose logs -f subscriptions &
+# uncomment the below line to ensure all messages get received
+#docker-compose logs -f subscriptions &
 docker-compose build --pull php-writer csharp-writer php-reader csharp-reader php-unit php-sub-validator
 docker-compose run php-unit
 docker-compose run php-writer
 docker-compose run csharp-writer
 docker-compose run php-reader
 docker-compose run csharp-reader
+echo waiting 10s for all events to be processed
+sleep 10
 docker-compose run php-sub-validator
